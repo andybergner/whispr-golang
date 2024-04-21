3e96a8c8725721b6da99b10d8ad2dcfc1d4dcd8a
Andy Gerome Bergner <gerome.andy@gmail.com>
2024-04-21 17:53:21 +0200

    1 one 1

--- go.mod
+++ go.mod
@@ -17,7 +17,6 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gomodule/redigo v1.9.2 // indirect
-	github.com/googollee/go-socket.io v1.7.0 // indirect
 	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"whispr-golang/helpers"
 	"whispr-golang/routes"
 
 	"github.com/gin-gonic/gin"
@@ -17,37 +19,132 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+type Client struct {
+	id		 string
+	conn 	 *websocket.Conn
+	send 	 chan []byte
+}
+
+type Message struct {
+	RecipientID 	string `json:"targetid"`
+	Content 	string `json:"content"`
+}
+
+type ClientManager struct {
+	clients		map[string]*Client
+	register	chan *Client
+	unregister	chan *Client
+}
+
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case client := <-manager.register:
+			manager.clients[client.id] = client
+			fmt.Println("Client connected:", client.id)
+		case client := <- manager.unregister:
+			if _, ok := manager.clients[client.id]; ok {
+
+				delete(manager.clients, client.id)
+				close(client.send)
+				fmt.Println("Client disconnected:", client.id)
+			}
+		}
+	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request, manager *ClientManager) {
+
+	clientToken := r.Header.Get("token")
+	if clientToken == "" {
+		http.Error(w, "No Authorization header provided", http.StatusUnauthorized)
+		return
+	}
+
+	claims, validateErr := helpers.ValidateToken(clientToken)
+	if validateErr != "" {
+		http.Error(w, validateErr, http.StatusInternalServerError)
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not open WebSocket connection", http.StatusBadRequest)
+		return
+	}
+
+	client := &Client{id: claims.Uid, conn: conn, send: make(chan []byte)}
+	manager.register <- client
+
+	go client.readPump(manager)
+	go client.writePump()
+
+}
+
+func (client *Client) readPump(manager *ClientManager) {
+	defer func() {
+		manager.unregister <- client
+		client.conn.Close()
+	}()
+	for {
+		_, message, err := client.conn.ReadMessage()
+		if err != nil {
+			manager.unregister <- client
+			client.conn.Close()
+			break
+		}
+
+		
+
+		var msg Message
+		if err := json.Unmarshal(message, &msg); err != nil {
+			fmt.Println("Error decoding message", err.Error())
+			continue
+		}
+
+
+		if msg.RecipientID != "" {
+			recipientClient, ok := manager.clients[msg.RecipientID]
+			if !ok {
+				fmt.Println("recipient client is not connected")
+				continue
+			}
+			select {
+			case recipientClient.send <- message:
+			default:
+				close(recipientClient.send)
+				delete(manager.clients, recipientClient.id)
+			}
+		} 
+	}
+}
+
+func (client *Client) writePump() {
+	defer func() {
+		client.conn.Close()
+	}()
+	for message := range client.send {
+		if err := client.conn.WriteMessage(websocket.TextMessage, message); err != nil {
+			return
+		}
+	}
+}
+
 func main() {
 
 	router := gin.Default()
 	router.Use(gin.Logger())
 
+	manager := ClientManager{
+		clients: make(map[string]*Client),
+		register: make(chan *Client),
+		unregister: make(chan *Client),
+	}
+
+	go manager.start()
+
 	router.GET("/ws", func(ctx *gin.Context) {
-		conns := make(map[*websocket.Conn]bool)
-		conn, err := upgrader.Upgrade(ctx.Writer, ctx.Request, nil)
-		if err != nil {
-			fmt.Println("upgrader error:", err.Error())
-			return
-		}
-		defer conn.Close()
-	
-		fmt.Println("new incoming connection from client:", conn.RemoteAddr())
-	
-		conns[conn] = true
-	
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println("read error:", err.Error())
-				delete(conns, conn)
-				return
-			}
-	
-			fmt.Println(string(msg))
-			if err := conn.WriteMessage(websocket.TextMessage, []byte("thank you for the msg!!!")); err != nil {
-				fmt.Println("write error:", err.Error())
-			}
-		}
+		handleConnections(ctx.Writer, ctx.Request, &manager)
 	})
 
 	routes.AuthRouter(router)
